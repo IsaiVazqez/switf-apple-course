cfc8e5fe080dd447fbc7ba93489f967270f16692
IsaiVazqez <7yuvryi@gmail.com>
2022-06-10 14:41:03 -0700

    Initial Commit

---
+++ Shared/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  Shared
+//
+//  Created by Isai on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
